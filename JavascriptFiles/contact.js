7424907d170784a51a7d263071fccf3e49bba840
astridlarsen <asla19ac@student.cbs.dk>
2019-10-18 11:11:46 +0200

    Tilføjelse af knapper

---
+++ JavascriptFiles/contact.js
@@ -0,0 +1,7 @@
+function validateContactInformation() {
+    var name = document.getElementById(`user_name`).value;
+
+
+ alert("Hi "+ name + "!"
+     + "Thank you for your inquiry! We will contact you as soon as possible.");
+}
