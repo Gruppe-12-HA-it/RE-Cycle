d3086bc8c78bd0cd1db2f8641848a5c9c0409c36
astridlarsen <asla19ac@student.cbs.dk>
2019-10-23 14:32:16 +0200

    COmmit of changes

--- JavascriptFiles/Login.js
+++ JavascriptFiles/Login.js
@@ -1,11 +1,34 @@
-function validate() {
-    var username = document.getElementById("username").value;
-    var password =document.getElementById("password").value;
-
-
-    if(username=="" || password=="" ){
-        alert("No blank allowed");
+// noen forhåndsdefinerte brukernavn:
+var objPeople =[
+    {
+        username: "astridlarsen",
+        password: "ha_it"
+    },
+    {
+        username: "mikkelfetterlein",
+        password: "mikkel28"
+    },
+    {
+        username: "sebastianteglhus",
+        password: "love2code"
     }
 
+]
+
+    function validate() {
+        var username = document.getElementById("username").value;
+        var password =document.getElementById("password").value;
+
+
+        if(username=="" || password=="" ){
+            alert("No blanks allowed");
+        }
+
+    for(i=0; i<objPeople.length; i++) {
+
+        if(username==objPeople[i].username&&password==objPeople[i].password) {
+            alert(username + " is logged in!")
+        }
+    }
 
 }
