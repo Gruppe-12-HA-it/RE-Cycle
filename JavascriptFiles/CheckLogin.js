4cc40a533296b3e886c758824c00083001e5d010
Sebteglhus <sete19ab@student.cbs.dk>
2019-11-07 17:16:40 +0100

    Adding login-check functions to profile, cart and add-to-cart. Moving around a few files, and fixing a few bugs and errors.

---
+++ JavascriptFiles/CheckLogin.js
@@ -0,0 +1,38 @@
+//checking login status when clicking profile logo in header
+document.getElementById('profile').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    //check if user is logged in; login stored as global token in localstorage
+    if(localStorage.user) {
+        location.href = "../HTMLFiles/Profile.html";
+        return; //cuts off the remainder of the if/else statement. If user isn't logged in, goes straight login.html below
+    }
+
+    location.href = "../HTMLFiles/Login.html";
+});
+console.log("CheckLogin.js loaded succesfully");
+
+//checking login status when clicking cart logo in header
+document.getElementById('cart').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if(localStorage.user) {
+        location.href="../HTMLFiles/Shoppingcart.html";
+        return;
+    }
+    location.href = "../HTMLFiles/LoginCart.html";
+
+});
+
+//Checking login status when attempting to add to cart
+document.getElementById('addToCart').addEventListener('click', function(event) {
+    event.preventDefault();
+//If user is logged in: add to cart. Else, display message and direct to login page
+    if(localStorage.user) {
+        //add to cart function here
+        return;
+    }
+    window.confirm("In order of adding products to your cart, you must log in or sign up.");
+    window.location.href='../HTMLFiles/Login.html';
+});
+
