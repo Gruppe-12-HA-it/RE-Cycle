02b928464b7239123e925a9d88707317601abe97
Seb <sete19ab@student.cbs.dk>
2019-11-08 11:34:35 +0100

    Fixing filter function v1

--- JavascriptFiles/CheckLogin.js
+++ JavascriptFiles/CheckLogin.js
@@ -41,6 +41,20 @@ Array.from(document.getElementsByClassName('addToCart')).forEach(element => {
 });
 
 //Checking that everything is loading as it should
+
+
+
+var mensFilter = document.getElementById('mensFilter');
+var womensFilter = document.getElementById('womensFilter');
+var childrensFilter = document.getElementById('childrensFilter');
+
+function handleChange() {
+    document.getElementById('mens').style.display = mensFilter.checked ? 'block' : 'none';
+    document.getElementById('womens').style.display = womensFilter.checked ? 'block' : 'none';
+    document.getElementById('childrens').style.display = childrensFilter.checked ? 'block' : 'none';
+}
+[mensFilter, womensFilter, childrensFilter].forEach(element => element.addEventListener('change', handleChange));
+
 console.log("Login check script successfully loaded.")
 
 
--- JavascriptFiles/Filter.js
+++ JavascriptFiles/Filter.js
@@ -1,5 +1,5 @@
-
-/*function mensFilter() {
+/*
+function mensFilter() {
     //Getting checkbox from products filter
     var mensFilter = document.getElementById('mensFilter');
     //getting output
@@ -13,7 +13,7 @@
     }
 }
 
- */
+
 var mensFilter = document.getElementById('mensFilter');
 var womensFilter = document.getElementById('womensFilter');
 var childrensFilter = document.getElementById('childrensFilter');
@@ -21,10 +21,12 @@ var childrensFilter = document.getElementById('childrensFilter');
 var productPage = document.getElementById('productPage');
 
 function handleChange() {
-    var currentlyChecked = [mensFilter, womensFilter, childrensFilter].map(element => element.checked);
-
-    result.innerHTML = JSON.stringify(currentlyChecked);
+    document.getElementById('mens').style.display = mensFilter.checked ? 'block' : 'none';
+    document.getElementById('womens').style.display = womensFilter.checked ? 'block' : 'none';
+    document.getElementById('childrens').style.display = childrensFilter.checked ? 'block' : 'none';
 }
 [mensFilter, womensFilter, childrensFilter].forEach(element => element.addEventListener('change', handleChange));
 
 console.log("Filter function succesfully loaded.");
+
+ */
