3e7f3844345193e6d0b29d94c7d2cf7083739f58
Seb <sete19ab@student.cbs.dk>
2019-12-03 10:29:30 +0100

    fix af css

--- JavascriptFiles/Productsjson.js
+++ JavascriptFiles/Productsjson.js
@@ -43,7 +43,7 @@ function createProductCard(product) {
             <h1>${product.name}</h1>
             <p>${product.description}</p>
             <p class="price">${product.price}</p>
-            <p><button type="button" class="addToCard">Add to Cart</button></p>
+            <p><button type="button" class="addToCard" data-id="${product.id}">Add to Cart</button></p>
         </div>
     `;
 }
