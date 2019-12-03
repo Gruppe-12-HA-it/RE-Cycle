6f9d9d5a20a69d610a880f927acaebe99d1ca5f6
LucasSimper <lusi19ac@student.cbs.dk>
2019-12-03 10:05:03 +0100

    Login Function - reworked - still needs more work

--- JavascriptFiles/Login.js
+++ JavascriptFiles/Login.js
@@ -1,34 +1,39 @@
-// noen forhåndsdefinerte brukernavn:
-var objPeople =[
-    {
-        username: "astridlarsen",
-        password: "ha_it"
-    },
-    {
-        username: "mikkelfetterlein",
-        password: "mikkel28"
-    },
-    {
-        username: "sebastianteglhus",
-        password: "love2code"
+var eMail = document.getElementById("enteredEmail");
+var password = document.getElementById("enteredPassword");
+var existingUser = JSON.parse(localStorage.getItem("allUsers"))
+// console.log(existingUser);
+
+//Admin login
+var adminEmail = "admin";
+var adminPassword = "admin";
+
+
+document.getElementById("Login!").addEventListener("click", checkLogin);
+
+function checkLogin (event) {
+    event.preventDefault();
+    console.log("works so far");
+    let userExists = false;
+
+     if (eMail.value == adminEmail && password.value == adminPassword) {
+        alert("Hello Admin");
+        userExists = true;
     }
 
-]
-
-    function validate() {
-        var username = document.getElementById("username").value;
-        var password =document.getElementById("password").value;
-
-
-        if(username=="" || password=="" ){
-            alert("No blanks allowed");
-        }
-
-    for(i=0; i<objPeople.length; i++) {
-
-        if(username==objPeople[i].username&&password==objPeople[i].password) {
-            alert(username + " is logged in!")
-        }
+  /*  else if (eMail.value == existingUser[i].eMail && password.value == existingUser[i].password) {
+        alert("Hello user")
+        userExists = true;
+        document.location = "GUI.html"
     }
 
+    else if (eMail.value == existingUser[i].eMail && password.value != existingUser[i].password) {
+        alert("Wrong password- try again!");
+        userExists = true;
+    }
+
+    */
+    if (userExists == false) {
+        alert ("It seems like you haven't created a user yet- we will direct you to our sign up page!");
+        location.href = "../HTMLFiles/Signup.html";
+    }
 }
