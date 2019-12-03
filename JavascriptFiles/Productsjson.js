9edc0ee9fc510b5af51ed6a1fc2e0dcab160bc4f
Seb <sete19ab@student.cbs.dk>
2019-12-03 14:31:45 +0100

    Shopping cart function started

--- JavascriptFiles/Productsjson.js
+++ JavascriptFiles/Productsjson.js
@@ -6,6 +6,7 @@ const childrensFilter = document.getElementById("childrensFilter");
 
 let data = undefined;
 
+
 function display(type) {
     if (!data) {
         fetch(myProducts)
@@ -25,7 +26,11 @@ function display(type) {
         data.forEach(function(product) {
             productsElement.innerHTML += createProductCard(product);
         });
+        Array.from(document.querySelectorAll('button[data-id]')).forEach(function(button) {
+            button.addEventListener('click', addToCart);
+        });
         return;
+
     }
 
     data
@@ -35,6 +40,15 @@ function display(type) {
         .forEach(function(product) {
             productsElement.innerHTML += createProductCard(product);
         });
+    Array.from(document.querySelectorAll('button[data-id]')).forEach(function(button) {
+        button.addEventListener('click', addToCart);
+    });
+}
+function addToCart() {
+    console.log(this.dataset.id);
+    this.remove();
+    localStorage.setItem("Order" ,this.dataset.id);
+
 }
 function createProductCard(product) {
     return `
@@ -43,22 +57,26 @@ function createProductCard(product) {
             <h1>${product.name}</h1>
             <p>${product.description}</p>
             <p class="price">${product.price}</p>
-            <p><button type="button" class="addToCard" data-id="${product.id}">Add to Cart</button></p>
+            <p><button type="button" class="addToCart" data-id="${product.id}" id="${product.id}">Add to Cart</button></p>
         </div>
     `;
 }
 
 function displayAll() {
     display();
+ButtonsLoaded = true;
 }
 function displayMens() {
     display("Mens");
+    ButtonsLoaded = true;
 }
 function displayWomens() {
     display("Womens");
+    ButtonsLoaded = true;
 }
 function displayChildrens() {
     display("Childrens");
+    ButtonsLoaded = true;
 }
 function displayMensWomens() {
     //potentielt skal jeg adde 3 eventlisteners (en per knap) og så tre if statement filters der viser den respektive data, derved stacker det dataen
